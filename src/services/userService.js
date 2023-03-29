0ff9476465f0b4e423e90b6f3f549dd74fa53136
phanvanviet0209 <entiviet@gmail.com>
2023-03-29 15:25:56 +0700

    complete 62

--- src/services/userService.js
+++ src/services/userService.js
@@ -116,7 +116,8 @@ let createNewUser = (data) => {
                     phonenumber: data.phonenumber,
                     gender: data.gender,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 })
                 resolve({
                     errCode: 0,
@@ -173,6 +174,9 @@ let updateUserData = (data) => {
                 user.positionId = data.positionId;
                 user.gender = data.gender;
                 user.phonenumber = data.phonenumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
 
                 await user.save();
                 resolve({
